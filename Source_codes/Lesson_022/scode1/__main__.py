29dc96098088629adbf773d4d16e116de08ee81b
Tsalmas Anastasios <tsalmanastasios@gmail.com>
2023-09-18 18:32:43 +0300

    Minor: Start lesson 22

---
+++ Source_codes/Lesson_022/scode1/__main__.py
@@ -0,0 +1,15 @@
+from game import SpaceRocks
+
+
+if __name__ == "__name__":
+    space_rocks = SpaceRocks()
+    space_rocks.main_loop()
+
+
+#
+#
+# Μπορούμε να τοποθετήσουμε τα αρχεία game.py & __main__.py στον φάκελο scode1.
+#
+# Μπορούμε να τρέξουμε αυτό τον κώδικα (αφού μεταβούμε στο παραπάνω directory), δίνοντας
+# την εντολή:
+#                       python space_rocks
